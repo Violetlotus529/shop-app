3d385e2023d82778c04a1aff63dc29c0333c722c
ren <matsuren0427@icloud.com>
2026-03-14 14:56:16 +0900

    fix stripe_refund_id index

--- db/migrate/20260221093731_add_refund_fields_to_orders.rb
+++ db/migrate/20260221093731_add_refund_fields_to_orders.rb
@@ -2,6 +2,6 @@ class AddRefundFieldsToOrders < ActiveRecord::Migration[7.1]
   def change
     add_column :orders, :refunded_at, :datetime
     add_column :orders, :stripe_refund_id, :string
-    add_index  :orders, :string_refund_id, unique: true
+    add_index  :orders, :stripe_refund_id, unique: true
   end
 end
