d37cf818082d3607b67f34bfd48313fdf0260ac8
ren <matsuren0427@icloud.com>
2026-03-14 14:34:03 +0900

    stripe_events

--- db/migrate/20260122121102_add_uniq_index_to_stripe_events.rb
+++ db/migrate/20260122121102_add_uniq_index_to_stripe_events.rb
@@ -1,7 +1,15 @@
 class AddUniqIndexToStripeEvents < ActiveRecord::Migration[7.1]
-  def change
+  def up
+    execute("DELETE FROM stripe_events")
+
     change_column_null :stripe_events, :event_id, false
     change_column_null :stripe_events, :event_type, false
     add_index :stripe_events, :event_id, unique: true
+  end
+
+  def down
+    remove_index :stripe_events, :event_id if index_exists?(:stripe_events, :event_id)
+    change_column_null :stripe_events, :event_type, true
+    change_column_null :stripe_events, :event_id, true
   end
 end
